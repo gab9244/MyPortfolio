f97ccdb776cabac850e459e138fb53e25b9e2626
Gabriel Francisco <gab9244@gmail.com>
2024-06-13 08:35:35 -0300

    changing styles

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -1,27 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import './index.css'
-import './componentes/Header.css'
-import './componentes/Home.css'
-import './componentes/About.css'
-import './componentes/Projects.css'
-import './componentes/Contact.css'
+import "./index.css";
+import "./componentes/Header.css";
+import "./componentes/Home.css";
+import "./componentes/About.css";
+import "./componentes/Projects.css";
+import "./componentes/Contact.css";
 
-
-import { Header } from './componentes/Header'
-import { Home } from './componentes/Home'
-import { About } from './componentes/About'
-import { Projects } from './componentes/Projects'
-import { Contact } from './componentes/Contact'
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import { Header } from "./componentes/Header";
+import { Home } from "./componentes/Home";
+import { About } from "./componentes/About";
+import { Projects } from "./componentes/Projects";
+import { Contact } from "./componentes/Contact";
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-
-    <Header/>
-    <Home/>
-    <About/>
-    <Projects/>
-    <Contact/>
-    
-  </React.StrictMode>,
-)
+    <Header />
+    <Home />
+    <About />
+    <Projects />
+    <Contact />
+  </React.StrictMode>
+);
