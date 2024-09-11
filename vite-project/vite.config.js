7ee43aa2a0543fb64181354ec7cc523b12fa4276
Gabriel Francisco <gab9244@gmail.com>
2024-09-11 15:31:41 -0300

    ..

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,17 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  build: {
-    rollupOptions: {
-      external: ['react-router-dom'],
-    },
-  },
-  resolve: {
-    alias: {
-      'react-router-dom': require.resolve('react-router-dom')
-    }
-  }
 })
